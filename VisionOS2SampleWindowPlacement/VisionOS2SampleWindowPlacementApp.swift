6eded1c68791c3fa42a52caa0f793ff14d6df8c7
tokufxug <tokufxug@gmail.com>
2024-07-05 00:21:08 +0900

    Initial Commit

---
+++ VisionOS2SampleWindowPlacement/VisionOS2SampleWindowPlacementApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS2SampleWindowPlacementApp.swift
+//  VisionOS2SampleWindowPlacement
+//
+//  Created by Sadao Tokuyama on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS2SampleWindowPlacementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionOS2SampleWindowPlacementTests/VisionOS2SampleWindowPlacementTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS2SampleWindowPlacementTests.swift
+//  VisionOS2SampleWindowPlacementTests
+//
+//  Created by Sadao Tokuyama on 7/5/24.
+//
+
+import Testing
+@testable import VisionOS2SampleWindowPlacement
+
+struct VisionOS2SampleWindowPlacementTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
